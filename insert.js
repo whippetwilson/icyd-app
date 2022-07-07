2f19c101fb810252ff15e3ebf22b8f44c8adf628
Charles Olupot <olupotcharles@gmail.com>
2022-07-08 00:02:57 +0300

    Changes

--- insert.js
+++ insert.js
@@ -5,6 +5,6 @@ const {
 const args = process.argv.slice(2);
 
 const processAll = async () => {
-  await processTrackedEntityInstancesAttributes(args[0], args[1], args[2]);
+  await processTrackedEntityInstances(args[0], args[1], args[2]);
 };
 processAll().then(() => console.log("Done"));
---
+++ stages.js
@@ -0,0 +1,31 @@
+const stages = [
+  "aTZwDRoJnxj",
+
+  "VzkQBBglj3O",
+
+  "KOFm3jJl7n7",
+
+  "sYE3K7fFM4Y",
+
+  "TuLJEpHu0um",
+
+  "LATgKmbf7Yv",
+
+  "EVkAS8LJNbO",
+
+  "B9EI27lmQrZ",
+
+  "HaaSLv2ur0l",
+
+  "SxnXrDtSJZp",
+
+  "yz3zh5IFEZm",
+
+  "kKlAyGUnCML",
+
+  "HEWq6yr4cs5",
+  "RDEklSXCD4C",
+  "IXxHJADVCkb",
+];
+
+console.log(stages.map((s) => s.toLowerCase()));
