9b9ae12298f35ef8e506f875e6e73924e40db045
Charles Olupot <olupotcharles@gmail.com>
2022-07-07 07:51:11 +0300

    Testing

--- services/es.service.js
+++ services/es.service.js
@@ -76,7 +76,7 @@ module.exports = {
     sql: {
       async handler(ctx) {
         const { query, ...others } = ctx.params;
-        console.log(others);
+        console.log(ctx.params);
         return await client.sql.query({
           query,
           ...others,
