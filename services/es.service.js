8b2b35d187de572c573ec02f68710f8c90bdccca
Charles Olupot <olupotcharles@gmail.com>
2022-08-06 20:08:36 +0300

    Changed to original

--- services/es.service.js
+++ services/es.service.js
@@ -1,7 +1,6 @@
 "use strict";
 const {Client} = require("@elastic/elasticsearch");
 const {flatMap} = require("lodash");
-
 const client = new Client({ node: "http://localhost:9200" });
 // const client = new Client({node: "http://192.168.64.3:9200"});
 
