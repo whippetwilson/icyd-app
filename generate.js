4a739ed98ce759ba3c0dd901c355688d667c4921
Charles Olupot <olupotcharles@gmail.com>
2022-08-06 09:22:16 +0300

    Testing

--- generate.js
+++ generate.js
@@ -1,14 +1,14 @@
 const {processTrackedEntityInstances, useTracker} = require("./process");
 const generate = async () => {
-	// try {
-	console.log("Working on HVAT");
-	await processTrackedEntityInstances("HEWq6yr4cs5", 100, 100);
-	console.log("Working on GROUP ACTIVITIES");
-	await processTrackedEntityInstances("IXxHJADVCkb", 100, 100);
-	console.log("Generating the layering");
-	await useTracker();
-	// } catch (error) {
-	// 	console.log(error.message);
-	// }
+	try {
+		// console.log("Working on HVAT");
+		// await processTrackedEntityInstances("HEWq6yr4cs5", 100, 100);
+		// console.log("Working on GROUP ACTIVITIES");
+		// await processTrackedEntityInstances("IXxHJADVCkb", 100, 100);
+		// console.log("Generating the layering");
+		await useTracker();
+	} catch (error) {
+		console.log(error.message);
+	}
 };
 generate().then(() => console.log("Done"));
