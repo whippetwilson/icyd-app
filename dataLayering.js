21f82bdcad98b68eb5b49266f465eb610b771b71
Charles Olupot <olupotcharles@gmail.com>
2022-08-16 14:01:10 +0300

    Adding some scheduling

--- dataLayering.js
+++ dataLayering.js
@@ -1,11 +1,4 @@
 const moment = require("moment");
 const {useTracker} = require("./process");
 
-useTracker([
-	moment().subtract(3, "quarters"),
-	moment().subtract(2, "quarters"),
-	moment().subtract(1, "quarters"),
-	moment()
-],
-["Dy5tmpdswEj"]
-).then(() => console.log("Done"));
+useTracker([moment().subtract(3, "quarters"), moment().subtract(2, "quarters"), moment().subtract(1, "quarters"), moment()], {lastUpdatedDuration: "10d"}).then(() => console.log("Done"));
