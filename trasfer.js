26cc182f5bdc362aef4440bb3deca0c437716e41
Charles Olupot <olupotcharles@gmail.com>
2023-02-21 17:20:40 +0300

    added more minutes

--- trasfer.js
+++ trasfer.js
@@ -1,11 +1,4 @@
 const schedule = require("node-schedule");
-const fs = require("fs");
-const {
-	parseISO,
-	differenceInHours,
-	differenceInMinutes,
-	differenceInDays,
-} = require("date-fns");
 const {
 	processTrackedEntityInstances,
 	useTracker,
@@ -13,80 +6,40 @@ const {
 	fetchUnits4Instances,
 	useLoader,
 } = require("./process");
-const moment = require("moment");
 const args = process.argv.slice(2);
 let running = {};
 const transfer = async (program) => {
-	if (!running[program]) {
-		running = { ...running, [program]: true };
-		let searches = {
-			last: moment().subtract(1, "quarters").format("YYYY-MM-DD"),
-		};
-		try {
-			searches = JSON.parse(fs.readFileSync(`./${program}.json`, "utf8"));
-			fs.writeFileSync(
-				`./${program}.json`,
-				JSON.stringify({ ...searches, last: new Date() })
-			);
-		} catch (e) {
-			fs.writeFileSync(
-				`./${program}.json`,
-				JSON.stringify({ ...searches, last: new Date() })
-			);
-			console.log(e);
+	try {
+		if (program === "RDEklSXCD4C") {
+			console.log("Fetching facilities");
+			const processedUnits = await fetchUnits4Instances();
+			console.log("Fetching metadata");
+			const { sessions } = await useLoader();
+			await processTrackedEntityInstances(program, 50, 100, useTracker, {
+				lastUpdatedDuration: "4h",
+				processedUnits,
+				sessions,
+			});
+		} else if (program === "IXxHJADVCkb") {
+			console.log("Fetching facilities");
+			const processedUnits = await fetchUnits4Instances();
+			console.log("Fetching metadata");
+			const { sessions } = await useLoader();
+			await processTrackedEntityInstances(program, 50, 100, useProgramStage, {
+				lastUpdatedDuration: "4h",
+				processedUnits,
+				sessions,
+			});
+		} else {
+			await processTrackedEntityInstances(program, 50, 100, null, {
+				lastUpdatedDuration: "4h",
+			});
 		}
-		let lastUpdatedDuration = "1m";
-		if (searches.last) {
-			const minutes = differenceInMinutes(new Date(), parseISO(searches.last));
-			if (minutes > 0 && minutes < 60) {
-				lastUpdatedDuration = `${minutes}m`;
-			} else if (minutes >= 60) {
-				const hours = differenceInHours(new Date(), parseISO(searches.last));
-				if (hours > 0 && hours < 24) {
-					lastUpdatedDuration = `${hours}h`;
-				} else if (hours >= 24) {
-					const days = differenceInDays(new Date(), parseISO(searches.last));
-					if (hours > 0 && hours < 24) {
-						lastUpdatedDuration = `${days}d`;
-					}
-				}
-			}
-		}
-		console.log(`Fetching for ${lastUpdatedDuration}`);
-		try {
-			if (program === "RDEklSXCD4C") {
-				console.log("Fetching facilities");
-				const processedUnits = await fetchUnits4Instances();
-				console.log("Fetching metadata");
-				const { sessions } = await useLoader();
-				await processTrackedEntityInstances(program, 50, 100, useTracker, {
-					lastUpdatedDuration,
-					processedUnits,
-					sessions,
-				});
-			} else if (program === "IXxHJADVCkb") {
-				console.log("Fetching facilities");
-				const processedUnits = await fetchUnits4Instances();
-				console.log("Fetching metadata");
-				const { sessions } = await useLoader();
-				await processTrackedEntityInstances(program, 50, 100, useProgramStage, {
-					lastUpdatedDuration,
-					processedUnits,
-					sessions,
-				});
-			} else {
-				await processTrackedEntityInstances(program, 50, 100, null, {
-					lastUpdatedDuration,
-				});
-			}
-		} catch (error) {
-			console.log(error.message);
-		}
-		running = { ...running, [program]: false };
-	} else {
-		console.log("Already running");
+	} catch (error) {
+		console.log(error.message);
 	}
+	running = { ...running, [program]: false };
 };
-schedule.scheduleJob("*/15 * * * * *", async () => {
+schedule.scheduleJob("*/30 * * * *", async () => {
 	await transfer(args[0]);
 });
